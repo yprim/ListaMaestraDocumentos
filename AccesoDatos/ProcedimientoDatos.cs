0b8b9f5ba588bb097db65842ab11cfac7c0cbc66
yprim <priscipo@gmail.com>
2019-04-25 08:17:18 -0600

    procedimiento servicios

--- AccesoDatos/ProcedimientoDatos.cs
+++ AccesoDatos/ProcedimientoDatos.cs
@@ -24,37 +24,80 @@ namespace AccesoDatos
 
         /// <summary>
         /// Priscilla Mena Monge
-        ///04/04/2019
-        /// Efecto: devuelve una lista con todos los Procedimientos
-        /// Requiere: -
+        ///22/04/2019
+        /// Efecto: devuelve una lista con todos los Procedimientos que están asignados a la opcion de sistema que está utilizando
+        /// Requiere: idDistema esto puede ser el sistema para laboratorio de ensayos, unidad de puentes o laboratorio de fuerza
         /// Modifica: -
         /// Devuelve: lista de Procedimientos
         /// </summary>
         /// <returns></returns>
-        public List<Procedimiento> getProcedimientos()
+        public List<Procedimiento> getProcedimientos( int idSistema)
         {
 
             List<Procedimiento> listaProcedimientos = new List<Procedimiento>();
 
-            SqlConnection sqlConnection = conexion.conexionLMD();
+            SqlConnection sqlConnexion = conexion.conexionLMD();
 
-            SqlCommand sqlCommand = new SqlCommand("select r.* from  Procedimiento r order by nombre_responsable;", sqlConnection);
+            String consulta = @"Select * from  Aprobador a , Responsable r ,Estado e, Procedimiento p, Sistema s
+                                                    where p.id_aprobador = a.id_aprobador and
+                                                    p.id_estado = e.id_estado and
+                                                    p.id_responsable = r.id_responsable and
+                                                     p.id_sistema = s.id_sistema
+                                                     and s.id_sistema = @id_sistema";
+
+            SqlCommand sqlCommand = new SqlCommand(consulta, sqlConnexion);
+            sqlCommand.Parameters.AddWithValue("@id_sistema", idSistema);
 
             SqlDataReader reader;
-            sqlConnection.Open();
+            sqlConnexion.Open();
             reader = sqlCommand.ExecuteReader();
 
             while (reader.Read())
             {
                 Procedimiento procedimiento = new Procedimiento();
+                Aprobador aprobador = new Aprobador();
+                Responsable responsable = new Responsable();
+                Estado estado = new Estado();
+                Sistema sistema = new Sistema();
 
-                procedimiento.idProcedimiento = Convert.ToInt32(reader["id_responsable"].ToString());
-                procedimiento.nombre = reader["nombre_responsable"].ToString();
+                //recupera procedimiento
+                procedimiento.idProcedimiento = Convert.ToInt32(reader["id_procedimiento"].ToString());
+                procedimiento.nombreDocumento = reader["nombre_documento_procedimiento"].ToString();
+                procedimiento.codigo = reader["codigo"].ToString();
+                procedimiento.acreditado = reader["acreditado"].ToString();
+                procedimiento.version = reader["version"].ToString();
+                procedimiento.rigeDesde = Convert.ToDateTime(reader["rige_desde"].ToString());
+                procedimiento.sustituyeA= reader["sustituye_a"].ToString();
+                procedimiento.distribuidoA = reader["distribuido_a"].ToString();
+                procedimiento.referenciaAdicional = reader["referencia_adicional"].ToString();
+                procedimiento.activo = reader["activo"].ToString();
 
+                //recupera aprobador
+
+                aprobador.idAprobador= Convert.ToInt32(reader["id_aprobador"].ToString());
+                aprobador.nombre = reader["nombre_aprobador"].ToString();
+
+                //recupera responsable
+                responsable.idResponsable= Convert.ToInt32(reader["id_responsable"].ToString());
+                responsable.nombre = reader["nombre_responsable"].ToString();
+
+                //recupera estado
+                estado.idEstado= Convert.ToInt32(reader["id_estado"].ToString());
+                estado.nombre = reader["nombre_estado"].ToString();
+
+                //recupera sistema
+                sistema.idSistema= Convert.ToInt32(reader["id_sistema"].ToString());
+                sistema.nombre = reader["nombre_sistema"].ToString();
+                procedimiento.aprobador = aprobador;
+                procedimiento.responsable = responsable;
+                procedimiento.estado = estado;
+                procedimiento.sistema = sistema;
+          
                 listaProcedimientos.Add(procedimiento);
+
             }
 
-            sqlConnection.Close();
+            sqlConnexion.Close();
 
             return listaProcedimientos;
         }
---
+++ Servicios/ProcedimientoServicios.cs
@@ -0,0 +1,40 @@
+﻿using AccesoDatos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    /// Priscilla Mena
+    /// 01/04/2019
+    /// Clase para administrar los servicios de Procedimiento
+    /// </summary>
+    public class ProcedimientoServicios
+    {
+        #region variables
+        ProcedimientoDatos procedimientoDatos = new ProcedimientoDatos();
+        #endregion
+
+        #region servicios
+
+        /// <summary>
+        /// Priscilla Mena Monge
+        ///25/04/2019
+        /// Efecto: devuelve una lista con todos los Procedimientos que están asignados a la opcion de sistema que está utilizando
+        /// Requiere: idDistema esto puede ser el sistema para laboratorio de ensayos, unidad de puentes o laboratorio de fuerza
+        /// Modifica: -
+        /// Devuelve: lista de Procedimientos
+        /// </summary>
+        /// <returns></returns>
+        public List<Procedimiento> getProcedimientos(int idSistema)
+        {
+            return procedimientoDatos.getProcedimientos(idSistema);
+        }
+
+        #endregion
+
+    }
+}
